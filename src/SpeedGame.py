5bd256387dd1f60e35a3d4a4d4da682fa10e0ff6
lesley-1076513 <143787726+lesley-1076513@users.noreply.github.com>
2023-09-22 20:11:12 +0200

    gitignore

--- src/SpeedGame.py
+++ src/SpeedGame.py
@@ -1,27 +1,4 @@
-import pygame as pg
+import game
 
-SCREEN_WIDTH = 1024
-SCREEN_HEIGHT = 768
-BG_COLOR = (100, 100, 100)
-FPS_RATE = 60
-
-pg.init()
-pg.display.set_caption("Speed Game")
-clock = pg.time.Clock()
-surface = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-running = True
-while running:
-    for event in pg.event.get():
-        match event.type:
-            case pg.QUIT:
-                running = False
-            case pg.KEYDOWN:
-                match event.key:
-                    case pg.K_ESCAPE:
-                        running = False
-
-    surface.fill(BG_COLOR)
-    pg.display.flip()
-    clock.tick(FPS_RATE)
-    
+if __name__ == "__main__":
+    game.run()
---
+++ src/game.py
@@ -0,0 +1,34 @@
+from enum import Enum
+import pygame as pg
+
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
+BG_COLOR = (100, 100, 100)
+FPS_RATE = 60
+
+class GameState(Enum):
+    TITLE = 1
+    MENU = 2
+    GAME = 3
+    
+pg.init()
+pg.display.set_caption("Speed Game")
+clock = pg.time.Clock()
+surface = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def run():
+    running = True
+    while running:
+        for event in pg.event.get():
+            match event.type:
+                case pg.QUIT:
+                    running = False
+                case pg.KEYDOWN:
+                    match event.key:
+                        case pg.K_ESCAPE:
+                            running = False
+
+        surface.fill(BG_COLOR)
+        pg.display.flip()
+        clock.tick(FPS_RATE)
+        
