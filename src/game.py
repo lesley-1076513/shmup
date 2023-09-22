b1f4a6b617e11a8cfd79e7a5bf9c8147aadc57e5
lesley-1076513 <143787726+lesley-1076513@users.noreply.github.com>
2023-09-22 21:25:29 +0200

    gamestate

--- src/game.py
+++ src/game.py
@@ -4,6 +4,7 @@ import pygame as pg
 class GameState(Enum):
     TITLE = 1
     GAME = 2
+    END = 3
 
 class Game():
     def __init__(self):
@@ -12,8 +13,9 @@ class Game():
         self.fps_rate = 60
         self.screen_width = 1024
         self.screen_height = 768
-        self.bg1 = (100, 100, 100)
-        self.bg2 = (0, 0, 0)
+        self.title_bg = (100, 100, 100)
+        self.game_bg = (0, 0, 0)
+        self.end_bg = (255, 0, 0)
 
 pg.init()
 clock = pg.time.Clock()
@@ -36,13 +38,17 @@ def run():
                             case GameState.TITLE:
                                 game.state = GameState.GAME
                             case GameState.GAME:
+                                game.state = GameState.END
+                            case GameState.END:
                                 game.state = GameState.TITLE
 
         match game.state:
             case GameState.TITLE:
-                surface.fill(game.bg1)
+                surface.fill(game.title_bg)
             case GameState.GAME:
-                surface.fill(game.bg2)
+                surface.fill(game.game_bg)
+            case GameState.END:
+                surface.fill(game.end_bg)
 
         pg.display.flip()
         clock.tick(game.fps_rate)
