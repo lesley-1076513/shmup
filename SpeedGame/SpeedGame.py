cfba0e65a36aa7c4f55046715a43f02965c17ac3
lesley-1076513 <143787726+lesley-1076513@users.noreply.github.com>
2023-09-22 19:13:16 +0200

    show window

--- SpeedGame/SpeedGame.py
+++ SpeedGame/SpeedGame.py
@@ -1,4 +1,26 @@
-import pygame
+import pygame as pg
 
-pygame.init()
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
+BG_COLOR = (100, 100, 100)
+FPS_RATE = 60
 
+pg.init()
+pg.display.set_caption("Speed Game")
+clock = pg.time.Clock()
+surface = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+running = True
+while running:
+    for event in pg.event.get():
+        match event.type:
+            case pg.QUIT:
+                running = False
+            case pg.KEYDOWN:
+                match event.key:
+                    case pg.K_ESCAPE:
+                        running = False
+
+    surface.fill(BG_COLOR)
+    pg.display.flip()
+    clock.tick(FPS_RATE)
